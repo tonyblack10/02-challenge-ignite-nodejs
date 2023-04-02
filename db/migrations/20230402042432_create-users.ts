61ff0d09860106bcabeeec539406255ede25c86f
Tony Costa <tonyblack10@gmail.com>
2023-04-02 01:45:39 -0300

    create users table

---
+++ db/migrations/20230402042432_create-users.ts
@@ -0,0 +1,15 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.string('name', 200).notNullable()
+    table.string('email', 200).notNullable()
+    table.string('password_hash', 255).notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
+}
---
+++ src/@types/knex.d.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    users: {
+      id: string
+      name: string
+      email: number
+      password_hash: string
+      created_at: string
+    }
+  }
+}
